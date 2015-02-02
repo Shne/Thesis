42cf082c2dcc0c781bb5392be525a47b920aeb48
Jan H. Knudsen <jan.h.knudsen@gmail.com>
2015-02-02 14:13:39 +0100

    precautionary commit

--- SimpleWaveletConstruction/Node.h
+++ SimpleWaveletConstruction/Node.h
@@ -8,13 +8,46 @@
 #ifndef NODE_H
 #define	NODE_H
 
+#include <string>
 #include <vector>
+//#include <set>
+//#include <array>
+//#include <forward_list>
 
-struct Node {
-    public:
+using namespace std;
+
+class Node {
+private:
         std::vector<bool> bitmap;
         Node* left;
         Node* right;
+public: 
+    Node();
+    Node(vector<char> s, char* alphabet, int alphabetSize) {
+        
+        char split = alphabet[alphabetSize/2];
+        char* leftAlphabet = alphabet;
+        int leftAlphabetSize = alphabetSize/2;
+        char* rightAlphabet = alphabet + leftAlphabetSize*sizeof(char);
+        int rightAlphabetSize = alphabetSize - leftAlphabetSize;
+
+        int i = 0;
+        vector<char> leftString;
+        vector<char> rightString;
+        for(auto it = s.cbegin(); it == s.cend(); it++) {
+            char c = *it;
+            if(c <= split) {
+                bitmap[i] = 0;
+                leftString.push_back(c);
+            } else {
+                bitmap[i] = 1;
+                rightString.push_back(c);
+            }
+            i++;
+        }
+        left = new Node(leftString, leftAlphabet, leftAlphabetSize);
+        right = new Node(rightString, rightAlphabet, rightAlphabetSize);
+    }
 };
 
 #endif	/* NODE_H */
--- SimpleWaveletConstruction/Tree.h
+++ SimpleWaveletConstruction/Tree.h
@@ -22,29 +22,8 @@ private:
     Node root;
     
 public:
-    Tree(vector<char> s, char* alphabet, int alphabetSize) {
-    char split = alphabet[alphabetSize/2];
-    char* leftAlphabet = alphabet;
-    int leftAlphabetSize = alphabetSize/2;
-    char* rightAlphabet = alphabet + leftAlphabetSize*sizeof(char);
-    int rightAlphabetSize = alphabetSize - leftAlphabetSize;
-
-    int i = 0;
-    vector<char> left;
-    vector<char> right;
-    for(auto it = s.cbegin(); it == s.cend(); it++) {
-        char c = *it;
-        if(c <= split) {
-            root.bitmap[i] = 0;
-            left.push_back(c);
-        } else {
-            root.bitmap[i] = 1;
-            right.push_back(c);
-        }
-        i++;
+    Tree() {
     }
-}
-
 };
 
 
