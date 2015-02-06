75010f9e36087e99a5bae3179109bb39c4148b35
Jan H. Knudsen <jan.h.knudsen@gmail.com>
2015-02-06 12:29:06 +0100

    updated run.sh script to new paths

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# ./SimpleWaveletConstruction/dist/Debug/GNU-Linux-x86/simplewaveletconstruction < Data/simpleConstruction_n:8_as:20.data
+# ./Code/Simple/Naive/dist/Debug/GNU-Linux-x86/simplewaveletconstruction < Data/simpleConstruction_n:8_as:20.data
 
-./SimpleWaveletConstructionSkewed/dist/Debug/GNU-Linux-x86/simplewaveletconstructionskewed < Data/simpleConstruction_n:8_as:20.data
+./Code/Simple/Naive/dist/Debug/GNU-Linux-x86/simplewaveletconstructionskewed < Data/simpleConstruction_n:8_as:20.data
