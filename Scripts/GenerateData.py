0497f188dfe7a34ba642d43f17a0df7c2a72a29f
Jan H. Knudsen <jan.h.knudsen@gmail.com>
2015-02-19 12:36:43 +0100

    GenerateData.py again generates Integer data

--- Scripts/GenerateData.py
+++ Scripts/GenerateData.py
@@ -44,4 +44,4 @@ def generateIntegerData(powerAmount, powerAlphabetSize):
 		randomArray.append(random.randint(0, alphabetSize-1))
 	randomArray.tofile(fd)
 
-generateReadableData(7, 12)
+generateIntegerData(7, 12)
