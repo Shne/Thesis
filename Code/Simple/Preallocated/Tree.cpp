ba0ac20f948b85409eb5186365ed393db63250e6
Roland42 <thealeran@gmail.com>
2015-02-23 15:50:45 +0100

    removed bit shrink test code

--- Code/Simple/Preallocated/Tree.cpp
+++ Code/Simple/Preallocated/Tree.cpp
@@ -9,7 +9,6 @@
 #include "Tree.h"
 #include <papi.h>
 
-#define NUM_EVENTS 3
 
 using namespace std;
 
@@ -37,7 +36,7 @@ Tree::Tree(vector<uint>* input, uint amount, uint alphabetSize, uint skew)
     Node* node_pt = static_cast<Node*> (::operator new (sizeof(Node)*2*alphabetSize));
     root = node_pt;
     
-    testShrinkToFit();
+    //testShrinkToFit();
     
     double dSkew = (double) skew;
     unsigned long bitmapSize = (unsigned long) amount * log2(alphabetSize) / log2(dSkew/(dSkew-1));
@@ -48,29 +47,7 @@ Tree::Tree(vector<uint>* input, uint amount, uint alphabetSize, uint skew)
     unsigned long bitmapOffset = 0;
 
     new (root) Node(input, 0, alphabetSize-1, nullptr, node_pt, bitmap, bitmapOffset, skew);
-    int Events[NUM_EVENTS] = { PAPI_TOT_CYC, PAPI_L1_TCM, PAPI_BR_MSP };
-    long_long values[NUM_EVENTS];
-    
-    long_long start_cycles, end_cycles, start_usec, end_usec;
-    int retval = PAPI_start_counters(Events, NUM_EVENTS);
-    if (retval != PAPI_OK) cout << "Start Error: " << retval << endl;
-    start_cycles = PAPI_get_real_cyc();
-    start_usec = PAPI_get_real_usec();
-    
     bitmap->shrink_to_fit();
-    
-    end_cycles = PAPI_get_real_cyc();
-    end_usec = PAPI_get_real_usec();
-    retval = PAPI_stop_counters(values, NUM_EVENTS);
-    if (retval != PAPI_OK) cout << "End Error: " << retval << endl;
-    
-    
-    
-    cout << "real cycles: " << end_cycles - start_cycles << endl; //real cycles
-    cout << "wall time in microseconds " << end_usec - start_usec << endl; //wall time in microseconds
-    cout << "cycles " << values[0] << endl; // cycles
-    cout << "cache misses " << values[1] << endl; // cache misses
-    cout << "branch misses " << values[2] << endl; // cache misses
     
 }
 
