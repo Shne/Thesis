5924688188ba765ea83533beec3c77171824d469
Jan H. Knudsen <jan.h.knudsen@gmail.com>
2015-02-27 10:12:57 +0100

    removed selfmade Bitmap that was nowhere near finished anyway

--- Code/Simple/NaiveInteger/nbproject/Makefile-Release.mk
+++ Code/Simple/NaiveInteger/nbproject/Makefile-Release.mk
@@ -35,7 +35,6 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
-	${OBJECTDIR}/Bitmap.o \
 	${OBJECTDIR}/Node.o \
 	${OBJECTDIR}/Tree.o \
 	${OBJECTDIR}/main.o
@@ -64,11 +63,6 @@ LDLIBSOPTIONS=-lpapi
 ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/naiveinteger: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/naiveinteger ${OBJECTFILES} ${LDLIBSOPTIONS}
-
-${OBJECTDIR}/Bitmap.o: Bitmap.cpp 
-	${MKDIR} -p ${OBJECTDIR}
-	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -I../../boost -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Bitmap.o Bitmap.cpp
 
 ${OBJECTDIR}/Node.o: Node.cpp 
 	${MKDIR} -p ${OBJECTDIR}
