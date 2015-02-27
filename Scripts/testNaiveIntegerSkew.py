eeeeaffd8ff834efbcc373cc99fdeb561b6c69aa
Roland42 <thealeran@gmail.com>
2015-02-27 12:48:46 +0100

    make clean in the correct way

--- Scripts/testNaiveIntegerSkew.py
+++ Scripts/testNaiveIntegerSkew.py
@@ -9,7 +9,7 @@ alphabetSize = 12
 program = "dist/Release/GNU-Linux-x86/naiveinteger"
 cwd = 'Code/Simple/NaiveInteger'
 
-subprocess.Popen(['make clean'], cwd=cwd).wait()
+subprocess.Popen(['make', 'clean'], cwd=cwd).wait()
 subprocess.Popen(['make'], cwd=cwd).wait()
 
 #print("Preallocated: Build \n")
@@ -21,17 +21,17 @@ subprocess.Popen(['make'], cwd=cwd).wait()
 # 		sleep(1)
 
 print("NaiveInteger: Rank \n")
-for skew in range(2,17):
-	for i in range(0, 5): #run 5 times for each skew
-		args = [program, str(amount), str(alphabetSize), str(skew), "rank"]
-		p = subprocess.Popen(args, cwd=cwd)
-		p.wait()
-		sleep(1)
+for skew in range(2,11):
+	#for i in range(0, 5): #run 5 times for each skew
+	args = [program, str(amount), str(alphabetSize), str(skew), "rank"]
+	p = subprocess.Popen(args, cwd=cwd)
+	p.wait()
+	sleep(1)
 
 print("NaiveInteger: Select \n")
-for skew in range(2,17):
-	for i in range(0, 5): #run 5 times for each skew
-		args = [program, str(amount), str(alphabetSize), str(skew), "select"]
-		p = subprocess.Popen(args, cwd=cwd)
-		p.wait()
-		sleep(1)
+for skew in range(2,11):
+	#for i in range(0, 5): #run 5 times for each skew
+	args = [program, str(amount), str(alphabetSize), str(skew), "select"]
+	p = subprocess.Popen(args, cwd=cwd)
+	p.wait()
+	sleep(1)
