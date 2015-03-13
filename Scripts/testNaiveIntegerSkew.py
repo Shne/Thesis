91969138ec271fa293ef1ecdd2833fbfe1cdd67e
Jan H. Knudsen <jan.h.knudsen@gmail.com>
2015-03-13 11:16:41 +0100

    testNaiveIntegerSkew: support a range of float skews

--- Scripts/testNaiveIntegerSkew.py
+++ Scripts/testNaiveIntegerSkew.py
@@ -13,6 +13,11 @@ outputFilename = 'Query_NaiveVsPreallocatedSkew16-1000queries.output'
 def addNewline():
 	open('Output/'+outputFilename, 'a').write('\n')
 
+def frange(x, y, jump):
+  while x < y:
+    yield x
+    x += jump
+
 subprocess.Popen(['make','CONF=Release', 'clean'], cwd=cwd).wait()
 subprocess.Popen(['make','CONF=Release'], cwd=cwd).wait()
 
@@ -27,9 +32,9 @@ subprocess.Popen(['make','CONF=Release'], cwd=cwd).wait()
 
 # addNewline()
 
-skewRange = 17
+skewRange = 6
 print("\nNaiveInteger: Rank")
-for skew in range(2,skewRange):
+for skew in frange(2,skewRange,0.2):
 	for i in range(0, 5): #run 5 times for each skew
 		args = [program, str(amount), str(alphabetSize), str(skew), 'rank', str(0), outputFilename]
 		subprocess.Popen(args, cwd=cwd).wait()
@@ -37,11 +42,11 @@ for skew in range(2,skewRange):
 		subprocess.Popen(args, cwd=cwd).wait()
 		args = [program, str(amount), str(alphabetSize), str(skew), 'rank', str(2), outputFilename]
 		subprocess.Popen(args, cwd=cwd).wait()
-	print(str(int((skew/(skewRange-1))*100)) + "%" + " finished\n")
+	# print(str(int((skew/(skewRange-1))*100)) + "%" + " finished\n")
 addNewline()
 
 print("\nNaiveInteger: Select")
-for skew in range(2,skewRange):
+for skew in frange(2,skewRange,0.2):
 	for i in range(0, 5): #run 5 times for each skew
 		args = [program, str(amount), str(alphabetSize), str(skew), 'select', str(0), outputFilename]
 		subprocess.Popen(args, cwd=cwd).wait()
@@ -49,7 +54,7 @@ for skew in range(2,skewRange):
 		subprocess.Popen(args, cwd=cwd).wait()
 		args = [program, str(amount), str(alphabetSize), str(skew), 'select', str(2), outputFilename]
 		subprocess.Popen(args, cwd=cwd).wait()
-	print(str(int((skew/(skewRange-1))*100)) + "%" + " finished\n")
+	# print(str(int((skew/(skewRange-1))*100)) + "%" + " finished\n")
 
 addNewline()
 addNewline()
