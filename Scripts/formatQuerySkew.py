644b07073020fce2434e788524bc0e66ee495239
Roland42 <thealeran@gmail.com>
2015-04-27 16:30:28 +0200

    updated skew query experiment format script to add error columns

--- Scripts/formatQuerySkew.py
+++ Scripts/formatQuerySkew.py
@@ -24,14 +24,13 @@ def getSkewsIncreaseFactor(skewList):
 			break
 	return float("{0:.1f}".format(stepsize))
 
-def getAverageValues(skewList, otherList):
-	stepsize = getNumberOfEqualSkews(skewList)
+def getAverageValuesForEachSkewInColumn(stepsize, columnList):
 	runningTimeAverage = []
 
-	for i in range(0, len(otherList), stepsize):
+	for i in range(0, len(columnList), stepsize):
 		temp = []
 		for j in range(i, stepsize+i):
-			temp.append(otherList[j])
+			temp.append(columnList[j])
 		average = numpy.average(numpy.array(temp))
 		runningTimeAverage.append(average)
 
@@ -68,18 +67,28 @@ def getRelativeStddev(stepsize, inputList):
 
 	return relativeStddevList
 
+def getStddev(stepsize, inputList):
+	stddevList = []
+
+	for i in range(0, len(inputList), stepsize):
+		dataWithSameSkew = []
+		for j in range(i, stepsize+i):
+			dataWithSameSkew.append(inputList[j])		
+		stddev = numpy.std(dataWithSameSkew)
+		stddevList.append(stddev)
+	return stddevList
 
 def getMaxRelativeStddevStr(columnNames, testDataFile, constructionAlg, test):
 	ReadOutput.getData(testDataFile, constructionAlg, test)	
 	dataTable = ReadOutput.getReadOutputLists(columnNames)
 	skewValues = dataTable[0]
 	dataTable.remove(skewValues)
-	uniqueSeperators = getUniqueSkewValues(skewValues)
+	uniqueSkewValues = getUniqueSkewValues(skewValues)
 	stepsize = getNumberOfEqualSkews(skewValues)
 
 	maxRelativeStddev = "0"
-	for dataRow in dataTable:
-		relativeStddev = getRelativeStddev(stepsize, dataRow)
+	for dataColumn in dataTable:
+		relativeStddev = getRelativeStddev(stepsize, dataColumn)
 		maxRelativeStddev += "   " + "{0:.3f}".format(max(relativeStddev)*100)
 	
 	ReadOutput.reset();
@@ -90,12 +99,12 @@ def getAvgRelativeStddevStr(columnNames, testDataFile, constructionAlg, test):
 	dataTable = ReadOutput.getReadOutputLists(columnNames)
 	skewValues = dataTable[0]
 	dataTable.remove(skewValues)
-	uniqueSeperators = getUniqueSkewValues(skewValues)
+	uniqueSkewValues = getUniqueSkewValues(skewValues)
 	stepsize = getNumberOfEqualSkews(skewValues)
 
 	maxRelativeStddev = "0"
-	for dataRow in dataTable:
-		relativeStddev = getRelativeStddev(stepsize, dataRow)
+	for dataColumn in dataTable:
+		relativeStddev = getRelativeStddev(stepsize, dataColumn)
 		maxRelativeStddev += "   " + "{0:.2f}".format(numpy.average(relativeStddev)*100)
 	
 	ReadOutput.reset();
@@ -104,26 +113,42 @@ def getAvgRelativeStddevStr(columnNames, testDataFile, constructionAlg, test):
 def writeToGnuplot(outputFile, dataTableKeys, testDataFile, constructionAlg, test, columns, i):
 	ReadOutput.getData(testDataFile, constructionAlg, test)
 	outputFile.write(columns+"\n")
-	dataTable = ReadOutput.getReadOutputLists(dataTableKeys)	
-	seperatingValues = dataTable[0]
-	uniqueSeperators = getUniqueSkewValues(seperatingValues)	
-	dataTable.remove(seperatingValues)
-	averageValueListList = []
 
-	for lst in dataTable:
-		averageValueListList.append(getAverageValues(seperatingValues, lst))
+	dataTable = ReadOutput.getReadOutputLists(dataTableKeys) #a list with lists of column data	
+	skewValues = dataTable[0]
+	uniqueSkewValues = getUniqueSkewValues(skewValues)
+	stepsize = getNumberOfEqualSkews(skewValues)
+	dataTable.remove(skewValues)
+
+	#convert DataTable to avg values in each column for each unique skew value
+	avgDataTable = []
+	for column in dataTable:
+		avgDataTable.append(getAverageValuesForEachSkewInColumn(stepsize, column))
+
+	#Add error columns
+	avgDataTable.append(getStddev(stepsize, ReadOutput.wallTimeList))
+	avgDataTable.append(getStddev(stepsize, ReadOutput.branchMissRateList))
+	avgDataTable.append(getStddev(stepsize, ReadOutput.branchMispredictionsList))
+	avgDataTable.append(getStddev(stepsize, ReadOutput.branchExecutedList))
+	avgDataTable.append(getStddev(stepsize, ReadOutput.TLBList))
+	avgDataTable.append(getStddev(stepsize, ReadOutput.l1DataCacheMissesList))
+	avgDataTable.append(getStddev(stepsize, ReadOutput.l2DataCacheMissesList))
+	avgDataTable.append(getStddev(stepsize, ReadOutput.l2DataCacheHitsList))
+	avgDataTable.append(getStddev(stepsize, ReadOutput.l3TotalCacheMissesList))
+	avgDataTable.append(getStddev(stepsize, ReadOutput.totalCyclesList))
 
 	outputFile.write(getMaxRelativeStddevStr(dataTableKeys, testDataFile, constructionAlg, test) + "\n")
 	outputFile.write(getAvgRelativeStddevStr(dataTableKeys, testDataFile, constructionAlg, test) + "\n")	
 	
 	index = 0
-	for skew in uniqueSeperators:
+	for skew in uniqueSkewValues:
 		strToWrite = "{0:.1f}".format(skew)
-		for averageValueList in averageValueListList:
-			if(averageValueListList.index(averageValueList) == 8 or averageValueListList.index(averageValueList) == 9):
-				strToWrite += "   " + str(float(averageValueList[index]))
-			else:
-				strToWrite += "   " + str(int(averageValueList[index]))
+		for column in avgDataTable: #take the same index in all column lists and write as row
+			strToWrite += "   " + str(float(column[index]))
+			# if(row.index(column) == 8 or row.index(column) == 9):
+			# 	strToWrite += "   " + str(float(column[index]))
+			# else:
+			# 	strToWrite += "   " + str(int(column[index]))
 		outputFile.write(strToWrite +"\n")
 		index+=1
 
@@ -139,7 +164,8 @@ naiveRankGnuplotFile = open("Report/Gnuplot/Data/naiveRankSkew.data", "w")
 naiveSelectGnuplotFile = open("Report/Gnuplot/Data/naiveSelectSkew.data", "w")
 controlledNodeMemoryRankGnuplotFile = open("Report/Gnuplot/Data/controlledNodeMemoryRankSkew.data", "w")
 controlledNodeMemorySelectGnuplotFile = open("Report/Gnuplot/Data/controlledNodeMemorySelectSkew.data", "w")
-columns = "#[skew]   [L1-DataCache-Misses]   [L2-DataCache-Misses]   [L3-TotalCache-Misses]   [Branch-Misses]   [TLB]   [Conditional Branches]   [Wall time (micro seconds)]   [L2-DataCache-Hits]   [BM Rate]   [L2 CM Rate]"
+columns = ("#[skew]   [L1-DataCache-Misses]   [L2-DataCache-Misses]   [L3-TotalCache-Misses]   [Branch-Misses]   [TLB]   [Conditional Branches]   [Wall time (micro seconds)]   [L2-DataCache-Hits]   [BM Rate]" + 
+"   [L2 CM Rate]  [WalltimeErr]  [BranchMissRateErr]  [BranchMisErr]  [BranchExeErr]  [TLBErr]  [L1CMErr]  [L2CMErr]  [L2CHErr]   [L3CMErr]   [CyclesErr]")
 
 testValueDataListKeys = ["skewList", 
 "l1DataCacheMissesList", 
