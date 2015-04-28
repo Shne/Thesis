77f7da9c162a12af9ad797ecc552f436b5eb0637
Jan H. Knudsen <jan.h.knudsen@gmail.com>
2015-04-28 15:57:16 +0200

    fixed up the graph for popcount: l2 cache hits and cm rate

--- Report/Gnuplot/popcountDifferenceNew.gnu
+++ Report/Gnuplot/popcountDifferenceNew.gnu
@@ -2,21 +2,20 @@
 set key top horizontal
 set term epslatex size 3.2,2.5 color font "" 6
 
-set xtics ("CPU Cycles" 0, "Wall Time" 3, "BM" 6, "TLBM" 9, "L1 CM" 12, "L2 CM" 15, "L3 CM" 18) rotate by -30
+set xtics ("CPU Cycles" 0, "Wall Time" 3, "BM" 6, "TLBM" 9, "L1 CM" 12, "L2 CM" 15, "L2 CHits" 18, "L2 CM Rate" 21, "L3 CM" 24) rotate by -30
 
 set ytics auto
 
 set boxwidth 1.0
 set style fill solid
 
-# set key font 'sans,2'
+set yrange [0:205]
 
+set key top left vertical
 set output 'popcountRankNew.tex'
 plot '../Data/popcountRankNew.data' every 3    using 1:2 title "Simple Binary Rank " with boxes ls 1,\
-     '../Data/popcountRankNew.data' every 3::1 using 1:2 title "Binary Rank using Popcount" with boxes ls 2
-
-set yrange [0:120]
+     '../Data/popcountRankNew.data' every 3::1 using 1:2 title "Binary Rank w/Popcount" with boxes ls 2
 
 set output 'popcountSelectNew.tex'
 plot '../Data/popcountSelectNew.data' every 3    using 1:2 title "Simple Binary Select" with boxes ls 1,\
-     '../Data/popcountSelectNew.data' every 3::1 using 1:2 title "Binary Select using Popcount" with boxes ls 2
+     '../Data/popcountSelectNew.data' every 3::1 using 1:2 title "Binary Select w/Popcount" with boxes ls 2
--- Scripts/formatPopcount.py
+++ Scripts/formatPopcount.py
@@ -24,6 +24,8 @@ RankBranchMisNoPop = avg(ReadOutput.branchMispredictionsList)
 RankTLBNoPop = avg(ReadOutput.TLBList)
 Rank1CMNoPop = avg(ReadOutput.l1DataCacheMissesList)
 Rank2CMNoPop = avg(ReadOutput.l2DataCacheMissesList)
+Rank2CHitsNoPop = avg(ReadOutput.l2DataCacheHitsList)
+Rank2CMRateNoPop = Rank2CMNoPop/Rank2CHitsNoPop
 Rank3CMNoPop = avg(ReadOutput.l3TotalCacheMissesList)
 
 # POPCOUNT
@@ -34,18 +36,30 @@ RankBranchMisPop = avg(ReadOutput.branchMispredictionsList)
 RankTLBPop = avg(ReadOutput.TLBList)
 Rank1CMPop = avg(ReadOutput.l1DataCacheMissesList)
 Rank2CMPop = avg(ReadOutput.l2DataCacheMissesList)
+Rank2CHitsPop = avg(ReadOutput.l2DataCacheHitsList)
+Rank2CMRatePop = Rank2CMPop/Rank2CHitsPop
 Rank3CMPop = avg(ReadOutput.l3TotalCacheMissesList)
 
-printList = [RankCyclesPop/RankCyclesNoPop, RankWalltimePop/RankWalltimeNoPop, RankBranchMisPop/RankBranchMisNoPop, RankTLBPop/RankTLBNoPop, Rank1CMPop/Rank1CMNoPop, Rank2CMPop/Rank2CMNoPop, Rank3CMPop/Rank3CMNoPop]
+printList = [
+	RankCyclesPop/RankCyclesNoPop, 
+	RankWalltimePop/RankWalltimeNoPop, 
+	RankBranchMisPop/RankBranchMisNoPop, 
+	RankTLBPop/RankTLBNoPop, 
+	Rank1CMPop/Rank1CMNoPop, 
+	Rank2CMPop/Rank2CMNoPop,
+	Rank2CHitsPop/Rank2CHitsNoPop,
+	Rank2CMRatePop/Rank2CMRateNoPop,
+	Rank3CMPop/Rank3CMNoPop
+]
 printList = [element*100 for element in printList]
 # Gnuplotfile
-for i in range(0,7):
+for i in range(len(printList)):
 	number = i*3
 	RankGnuplotFile.write(str(number) + '\t' + '100' + '\n')
 	RankGnuplotFile.write(str(number+1) + '\t' + str(printList[i]) + '\n')
 	RankGnuplotFile.write(str(number+2) + '\t' + '\n')
 
-value = "{:,.1f}"
+value = "{:,.2f}"
 percent = "{:.3f}"
 
 # Values file
@@ -58,7 +72,9 @@ ValuesFile.write('\\textbf{BM} & '+value.format(RankBranchMisNoPop)+' & '+value.
 ValuesFile.write('\\textbf{TLBM} & '+value.format(RankTLBNoPop)+' & '+value.format(RankTLBPop)+' & '+percent.format(printList[3])+'\\% \\\\ \hline\n')
 ValuesFile.write('\\textbf{L1 CM} & '+value.format(Rank1CMNoPop)+' & '+value.format(Rank1CMPop)+' & '+percent.format(printList[4])+'\\% \\\\ \hline\n')
 ValuesFile.write('\\textbf{L2 CM} & '+value.format(Rank2CMNoPop)+' & '+value.format(Rank2CMPop)+' & '+percent.format(printList[5])+'\\% \\\\ \hline\n')
-ValuesFile.write('\\textbf{L3 CM} & '+value.format(Rank3CMNoPop)+' & '+value.format(Rank3CMPop)+' & '+percent.format(printList[6])+'\\% \\\\ \hline\n')
+ValuesFile.write('\\textbf{L2 CHits} & '+value.format(Rank2CHitsNoPop)+' & '+value.format(Rank2CHitsPop)+' & '+percent.format(printList[6])+'\\% \\\\ \hline\n')
+ValuesFile.write('\\textbf{L2 CM Rate} & '+value.format(Rank2CMRateNoPop)+' & '+value.format(Rank2CMRatePop)+' & '+percent.format(printList[7])+'\\% \\\\ \hline\n')
+ValuesFile.write('\\textbf{L3 CM} & '+value.format(Rank3CMNoPop)+' & '+value.format(Rank3CMPop)+' & '+percent.format(printList[8])+'\\% \\\\ \hline\n')
 ValuesFile.write('\\end{tabular}\\\\[5pt]\n')
 
 
@@ -70,6 +86,8 @@ SelectBranchMisNoPop = avg(ReadOutput.branchMispredictionsList)
 SelectTLBNoPop = avg(ReadOutput.TLBList)
 Select1CMNoPop = avg(ReadOutput.l1DataCacheMissesList)
 Select2CMNoPop = avg(ReadOutput.l2DataCacheMissesList)
+Select2CHitsNoPop = avg(ReadOutput.l2DataCacheHitsList)
+Select2CMRateNoPop = Select2CMNoPop/Select2CHitsNoPop
 Select3CMNoPop = avg(ReadOutput.l3TotalCacheMissesList)
 
 ReadOutput.getData(testDataFile, "SimpleNaiveInteger", "select")
@@ -79,13 +97,25 @@ SelectBranchMisPop = avg(ReadOutput.branchMispredictionsList)
 SelectTLBPop = avg(ReadOutput.TLBList)
 Select1CMPop = avg(ReadOutput.l1DataCacheMissesList)
 Select2CMPop = avg(ReadOutput.l2DataCacheMissesList)
+Select2CHitsPop = avg(ReadOutput.l2DataCacheHitsList)
+Select2CMRatePop = Select2CMPop/Select2CHitsPop
 Select3CMPop = avg(ReadOutput.l3TotalCacheMissesList)
 
-printList = [SelectCyclesPop/SelectCyclesNoPop, SelectWalltimePop/SelectWalltimeNoPop, SelectBranchMisPop/SelectBranchMisNoPop, SelectTLBPop/SelectTLBNoPop, Select1CMPop/Select1CMNoPop, Select2CMPop/Select2CMNoPop, Select3CMPop/Select3CMNoPop]
+printList = [
+	SelectCyclesPop/SelectCyclesNoPop, 
+	SelectWalltimePop/SelectWalltimeNoPop, 
+	SelectBranchMisPop/SelectBranchMisNoPop, 
+	SelectTLBPop/SelectTLBNoPop, 
+	Select1CMPop/Select1CMNoPop, 
+	Select2CMPop/Select2CMNoPop, 
+	Select2CHitsPop/Select2CHitsNoPop, 
+	Select2CMRatePop/Select2CMRateNoPop,
+	Select3CMPop/Select3CMNoPop
+]
 printList = [element*100 for element in printList]
 
 #gnuplot
-for i in range(0,7):
+for i in range(len(printList)):
 	number = i*3
 	SelectGnuplotFile.write(str(number) + '\t' + '100' + '\n')
 	SelectGnuplotFile.write(str(number+1) + '\t' + str(printList[i]) + '\n')
@@ -101,7 +131,9 @@ ValuesFile.write('\\textbf{BM} & '+value.format(SelectBranchMisNoPop)+' & '+valu
 ValuesFile.write('\\textbf{TLBM} & '+value.format(SelectTLBNoPop)+' & '+value.format(SelectTLBPop)+' & '+percent.format(printList[3])+'\\% \\\\ \hline\n')
 ValuesFile.write('\\textbf{L1 CM} & '+value.format(Select1CMNoPop)+' & '+value.format(Select1CMPop)+' & '+percent.format(printList[4])+'\\% \\\\ \hline\n')
 ValuesFile.write('\\textbf{L2 CM} & '+value.format(Select2CMNoPop)+' & '+value.format(Select2CMPop)+' & '+percent.format(printList[5])+'\\% \\\\ \hline\n')
-ValuesFile.write('\\textbf{L3 CM} & '+value.format(Select3CMNoPop)+' & '+value.format(Select3CMPop)+' & '+percent.format(printList[6])+'\\% \\\\ \hline\n')
+ValuesFile.write('\\textbf{L2 CM} & '+value.format(Select2CHitsNoPop)+' & '+value.format(Select2CHitsPop)+' & '+percent.format(printList[6])+'\\% \\\\ \hline\n')
+ValuesFile.write('\\textbf{L2 CM Rate} & '+value.format(Select2CMRateNoPop)+' & '+value.format(Select2CMRatePop)+' & '+percent.format(printList[7])+'\\% \\\\ \hline\n')
+ValuesFile.write('\\textbf{L3 CM} & '+value.format(Select3CMNoPop)+' & '+value.format(Select3CMPop)+' & '+percent.format(printList[8])+'\\% \\\\ \hline\n')
 ValuesFile.write('\\end{tabular}\\\\[5pt]\n')
 
 
