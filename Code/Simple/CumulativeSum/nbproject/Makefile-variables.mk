6fda740a40f7bdf8b445c46bf173233f4d094bcf
Jan H. Knudsen <jan.h.knudsen@gmail.com>
2015-05-04 13:31:32 +0200

    branchless select build configuration for cumulativesum

--- Code/Simple/CumulativeSum/nbproject/Makefile-variables.mk
+++ Code/Simple/CumulativeSum/nbproject/Makefile-variables.mk
@@ -22,6 +22,14 @@ CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/cumulativesum
 CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
 CND_PACKAGE_NAME_Release=cumulativesum.tar
 CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/cumulativesum.tar
+# ReleaseBranchlessSelect configuration
+CND_PLATFORM_ReleaseBranchlessSelect=GNU-Linux-x86
+CND_ARTIFACT_DIR_ReleaseBranchlessSelect=dist/ReleaseBranchlessSelect/GNU-Linux-x86
+CND_ARTIFACT_NAME_ReleaseBranchlessSelect=cumulativesum
+CND_ARTIFACT_PATH_ReleaseBranchlessSelect=dist/ReleaseBranchlessSelect/GNU-Linux-x86/cumulativesum
+CND_PACKAGE_DIR_ReleaseBranchlessSelect=dist/ReleaseBranchlessSelect/GNU-Linux-x86/package
+CND_PACKAGE_NAME_ReleaseBranchlessSelect=cumulativesum.tar
+CND_PACKAGE_PATH_ReleaseBranchlessSelect=dist/ReleaseBranchlessSelect/GNU-Linux-x86/package/cumulativesum.tar
 #
 # include compiler specific variables
 #
