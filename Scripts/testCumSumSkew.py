9667467a6e5ab68e31a8e0eb308ffd1a61c76145
Roland42 <thealeran@gmail.com>
2015-06-11 14:32:09 +0200

    redid cumsumskew rank for skew 2, 2.4, 6

--- Scripts/testCumSumSkew.py
+++ Scripts/testCumSumSkew.py
@@ -47,39 +47,6 @@ def writeMassifData(programName, skew):
 	f.close()
 
 
-
-
-# blockSizeList = []
-# walltimeList = []
-# def WriteData(i, testsPerSize):
-# 	startIndex = i*testsPerSize
-# 	endIndex = startIndex + testsPerSize
-# 	blockSizeList.append(avg(ReadOutput.blockSizeList[startIndex:endIndex]))
-# 	walltimeList.append(avg(ReadOutput.wallTimeList[startIndex:endIndex]))
-
-# bestBlockSizeFile = open("Output/bestCumSumQueryBlockSize.output", "w")
-
-# #Get best rank block size
-# ReadOutput.getData('Output/CumulativeSumBlockSizeZoomedRank_n8as16_1000Queries.output', "CumulativeSum", "rank")
-# for i in range(0, 40):
-# 	WriteData(i, 5)
-# ReadOutput.reset()
-# index = np.argmax(np.array(walltimeList))
-# blockSizeRank = int(blockSizeList[index])
-# bestBlockSizeFile.write('Rank: ' + str(blockSizeRank) +', ' + str(walltimeList[index]) + '\n')
-
-
-# #Get best select block size.
-# ReadOutput.getData('Output/CumulativeSumBlockSize_n8as16_1000Queries.output', "CumulativeSum", "select")
-# blockSizeList = []
-# walltimeList = []
-# for i in range(0, 40):
-# 	WriteData(i, 5)
-# ReadOutput.reset()
-# index = np.argmax(np.array(walltimeList))
-# blockSizeSelect = int(blockSizeList[index])
-# bestBlockSizeFile.write('Select: ' + str(blockSizeSelect) +', ' + str(walltimeList[index]) + '\n')
-
 blockSizeRank = 64/8
 blockSizeSelect = 2048/8
 blockSizeBuild = 1024/8
@@ -93,10 +60,16 @@ subprocess.Popen(['make','CONF=Release', 'clean'], cwd=cumulativeSumCwd).wait()
 subprocess.Popen(['make','CONF=Release'], cwd=cumulativeSumCwd).wait()
 
 
+addNewline()
+addNewline()
+addNewline()
 
+
+skewRange = [2.0, 2.4, 6.0]
 print("CumulativeSum: Rank \n")
 test = 'rank'
-for skew in frange(skewStart, skewEnd, skewStep):
+# for skew in frange(skewStart, skewEnd, skewStep):
+for skew in skewRange:
 	for _ in range(repeats):
 		args = [cumulativeSumProgram, str(amount), str(alphabetSize), test, str(0), str(blockSizeRank), outputFilename, clampFloat(skew)]
 		subprocess.Popen(args, cwd=cumulativeSumCwd).wait()
@@ -107,46 +80,46 @@ for skew in frange(skewStart, skewEnd, skewStep):
 
 addNewline()
 
-print("CumulativeSum: Select \n")
-test = 'select'
-for skew in frange(skewStart, skewEnd, skewStep):
-	for _ in range(repeats):
-		args = [cumulativeSumProgram, str(amount), str(alphabetSize), test, str(0), str(blockSizeSelect), outputFilename, clampFloat(skew)]
-		subprocess.Popen(args, cwd=cumulativeSumCwd).wait()
-		args = [cumulativeSumProgram, str(amount), str(alphabetSize), test, str(1), str(blockSizeSelect), outputFilename, clampFloat(skew)]
-		subprocess.Popen(args, cwd=cumulativeSumCwd).wait()
-		args = [cumulativeSumProgram, str(amount), str(alphabetSize), test, str(2), str(blockSizeSelect), outputFilename, clampFloat(skew)]
-		subprocess.Popen(args, cwd=cumulativeSumCwd).wait()
+# print("CumulativeSum: Select \n")
+# test = 'select'
+# for skew in frange(skewStart, skewEnd, skewStep):
+# 	for _ in range(repeats):
+# 		args = [cumulativeSumProgram, str(amount), str(alphabetSize), test, str(0), str(blockSizeSelect), outputFilename, clampFloat(skew)]
+# 		subprocess.Popen(args, cwd=cumulativeSumCwd).wait()
+# 		args = [cumulativeSumProgram, str(amount), str(alphabetSize), test, str(1), str(blockSizeSelect), outputFilename, clampFloat(skew)]
+# 		subprocess.Popen(args, cwd=cumulativeSumCwd).wait()
+# 		args = [cumulativeSumProgram, str(amount), str(alphabetSize), test, str(2), str(blockSizeSelect), outputFilename, clampFloat(skew)]
+# 		subprocess.Popen(args, cwd=cumulativeSumCwd).wait()
 
-regex = b"mem_heap_B=(?P<heap>\d+)\nmem_heap_extra_B=(?P<extra>\d+)\nmem_stacks_B=(?P<stacks>\d+)\nheap_tree=empty$"
-compiledRegex = re.compile(regex)
+# regex = b"mem_heap_B=(?P<heap>\d+)\nmem_heap_extra_B=(?P<extra>\d+)\nmem_stacks_B=(?P<stacks>\d+)\nheap_tree=empty$"
+# compiledRegex = re.compile(regex)
 
-addNewline()
+# addNewline()
 
-print("CumulativeSum: Build Time\n")
-test = 'build'
-for skew in frange(skewStart, skewEnd, skewStep):
-	for _ in range(repeats):
-		args = [cumulativeSumProgram, str(amount), str(alphabetSize), test, str(0), str(blockSizeBuild), outputFilename, clampFloat(skew)]
-		subprocess.Popen(args, cwd=cumulativeSumCwd).wait()
-		args = [cumulativeSumProgram, str(amount), str(alphabetSize), test, str(1), str(blockSizeBuild), outputFilename, clampFloat(skew)]
-		subprocess.Popen(args, cwd=cumulativeSumCwd).wait()
-		args = [cumulativeSumProgram, str(amount), str(alphabetSize), test, str(2), str(blockSizeBuild), outputFilename, clampFloat(skew)]
-		subprocess.Popen(args, cwd=cumulativeSumCwd).wait()
+# print("CumulativeSum: Build Time\n")
+# test = 'build'
+# for skew in frange(skewStart, skewEnd, skewStep):
+# 	for _ in range(repeats):
+# 		args = [cumulativeSumProgram, str(amount), str(alphabetSize), test, str(0), str(blockSizeBuild), outputFilename, clampFloat(skew)]
+# 		subprocess.Popen(args, cwd=cumulativeSumCwd).wait()
+# 		args = [cumulativeSumProgram, str(amount), str(alphabetSize), test, str(1), str(blockSizeBuild), outputFilename, clampFloat(skew)]
+# 		subprocess.Popen(args, cwd=cumulativeSumCwd).wait()
+# 		args = [cumulativeSumProgram, str(amount), str(alphabetSize), test, str(2), str(blockSizeBuild), outputFilename, clampFloat(skew)]
+# 		subprocess.Popen(args, cwd=cumulativeSumCwd).wait()
 
-addNewline()
+# addNewline()
 
-print("CumulativeSum: Build Memory \n")
-test = 'buildmemory'
-blockSize = blockSizeSelect
-for skew in frange(skewStart, skewEnd, skewStep):
-	for _ in range(repeats):
-		args = massif + [cumulativeSumProgram, str(amount), str(alphabetSize), test, str(0), str(blockSizeBuild), outputFilename, clampFloat(skew)]
-		subprocess.Popen(args, cwd=cumulativeSumCwd).wait()
-		writeMassifData('CumulativeSum', skew)
+# print("CumulativeSum: Build Memory \n")
+# test = 'buildmemory'
+# blockSize = blockSizeSelect
+# for skew in frange(skewStart, skewEnd, skewStep):
+# 	for _ in range(repeats):
+# 		args = massif + [cumulativeSumProgram, str(amount), str(alphabetSize), test, str(0), str(blockSizeBuild), outputFilename, clampFloat(skew)]
+# 		subprocess.Popen(args, cwd=cumulativeSumCwd).wait()
+# 		writeMassifData('CumulativeSum', skew)
 
-os.remove(tempOutFile)
-os.remove(manualSnapshotFileName)
+# os.remove(tempOutFile)
+# os.remove(manualSnapshotFileName)
 
 
 
